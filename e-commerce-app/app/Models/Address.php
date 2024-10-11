927c27df44c6d8179070409affd2d18f01376af0
Duy <vuducduy1012@gmail.com>
2024-10-11 23:31:21 +0700

    Migrations and Models (Not user)

---
+++ e-commerce-app/app/Models/Address.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'first_name',
+        'last_name',
+        'phone',
+        'address',
+        'city',
+        'state',
+        'zip_code',
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFullAddressAttribute(): string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+}
